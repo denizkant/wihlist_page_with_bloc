44649a59680a56ab004863e37120392a3384e432
denizkant <86944888+denizkant@users.noreply.github.com>
2023-12-17 23:04:55 +0300

    17.12.2023

--- lib/features/home/ui/home.dart
+++ lib/features/home/ui/home.dart
@@ -1,5 +1,6 @@
 import 'package:bloc_deneme/features/cart/ui/cart.dart';
 import 'package:bloc_deneme/features/home/bloc/home_bloc.dart';
+import 'package:bloc_deneme/features/home/ui/product_tile_widget.dart';
 import 'package:bloc_deneme/features/wishlist/ui/wishlist.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -35,24 +36,47 @@ class _HomeState extends State<Home> {
         }
       },
       builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.teal,
-            title: const Text('Akshit Grocery App'),
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    homeBloc.add(HomeWishListButtonNavigateEvent());
-                  },
-                  icon: Icon(Icons.favorite)),
-              IconButton(
-                  onPressed: () {
-                    homeBloc.add(HomeCartButtonNavigateEvent());
-                  },
-                  icon: Icon(Icons.shopping_bag_outlined)),
-            ],
-          ),
-        );
+        switch (state.runtimeType) {
+          case HomeLoadingState:
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+
+          case HomeLoadedSuccessState:
+            final succesSatte = state as HomeLoadedSuccessState;
+            return Scaffold(
+                appBar: AppBar(
+                  backgroundColor: Colors.teal,
+                  title: const Text('Akshit Grocery App'),
+                  actions: [
+                    IconButton(
+                        onPressed: () {
+                          homeBloc.add(HomeWishListButtonNavigateEvent());
+                        },
+                        icon: Icon(Icons.favorite)),
+                    IconButton(
+                        onPressed: () {
+                          homeBloc.add(HomeCartButtonNavigateEvent());
+                        },
+                        icon: Icon(Icons.shopping_bag_outlined)),
+                  ],
+                ),
+                body: ListView.builder(
+                    itemCount: succesSatte.products.length,
+                    itemBuilder: (context, index) {
+                      return ProductTileWidget(
+                          productDataModel: succesSatte.products[index]);
+                    }));
+
+          case HomeErrorstate:
+            return Scaffold(
+              body: Center(child: Text('Error')),
+            );
+          default:
+            return SizedBox();
+        }
       },
     );
   }
---
+++ lib/features/home/ui/product_tile_widget.dart
@@ -0,0 +1,28 @@
+import 'package:bloc_deneme/features/home/model/home_product_data_model.dart';
+import 'package:flutter/material.dart';
+
+class ProductTileWidget extends StatelessWidget {
+  final ProductDataModel productDataModel;
+  const ProductTileWidget({super.key, required this.productDataModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Container(
+            height: 200,
+            width: double.maxFinite,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(productDataModel.imageUrl))),
+          ),
+          Text(productDataModel.name),
+          Text(productDataModel.description),
+        ],
+      ),
+    );
+  }
+}
